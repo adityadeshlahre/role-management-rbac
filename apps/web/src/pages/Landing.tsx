a2c3557ba792b696c5bf9884f5bfd03b1236d8d8
Aditya Deshlahre <wymaditya@gmail.com>
2025-06-06 22:23:15 +0530

    Initial commit

---
+++ apps/web/src/pages/Landing.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@repo/ui/button";
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <>
+      <div className="text-8xl">Quick + Start</div>
+      <Button
+        appName="APP BUTTON CLICKED"
+        className="text-6xl bg-lime-200 m-2 border-2 rounded-2xl border-lime-500 hover:bg-lime-300 hover:border-lime-600"
+      >
+        CLICK
+      </Button>
+    </>
+  );
+};
+
+export default Landing;
