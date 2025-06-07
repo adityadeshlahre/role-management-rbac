0b32f6dac9498b5b559c017dab8712e023b5ba9e
Aditya Deshlahre <wymaditya@gmail.com>
2025-06-08 04:47:44 +0530

    fix: cicd trigger + test db seed

--- apps/web/src/utils/base.ts
+++ apps/web/src/utils/base.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL: string = import.meta.env.SERVER_URL || "https://rbac-server.onrender.com";
+export const SERVER_URL: string = import.meta.env.SERVER_URL || "https://rbac-server-x02l.onrender.com";
