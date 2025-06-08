2d4adec3e20f023edf9b847365ff156a9a6d1559
Aditya Deshlahre <wymaditya@gmail.com>
2025-06-08 10:16:33 +0530

    fix: server base url fix

--- apps/web/src/services/axios.ts
+++ apps/web/src/services/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { SERVER_URL } from "../utils/base";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || SERVER_URL || "https://rbac-server-x02l.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
